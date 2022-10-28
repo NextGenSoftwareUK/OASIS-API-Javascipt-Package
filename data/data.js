df8a7a8e3220e6ad42487a241aad1729da695f8d
Derek David <derekmiracledavid@hotmail.com>
2022-10-28 23:59:05 +0100

    data api updated

--- data/data.js
+++ data/data.js
@@ -8,22 +8,22 @@ class Data {
 
   async loadHolon(
     data = {
-      id: "188e0e27-3af2-478f-af86-030c56a42edb",
-      LoadChildren: false,
-      Recursive: false,
-      MaxChildDepth: 1,
-      ContinueOnError: false,
-      Version: 1,
-      ProviderType: "HoloOASIS",
-      SetGlobally: false,
-      ShowDetailedSettings: true,
-      AutoFailOverEnabled: "true",
-      AutoReplicationEnabled: "false",
-      AutoLoadBalanceEnabled: "false",
-      AutoFailOverProviders: "MongoDBOASIS, HoloOASIS",
-      AutoReplicationProviders: "MongoDBOASIS, HoloOASIS",
-      AutoLoadBalanceProviders: "MongoDBOASIS, HoloOASIS",
-      WaitForAutoReplicationResult: false,
+      providerType: "Default",
+      setGlobally: true,
+      autoFailOverMode: "DEFAULT",
+      autoReplicationMode: "DEFAULT",
+      autoLoadBalanceMode: "DEFAULT",
+      autoFailOverProviders: "DEFAULT",
+      autoReplicationProviders: "DEFAULT",
+      autoLoadBalanceProviders: "DEFAULT",
+      waitForAutoReplicationResult: false,
+      showDetailedSettings: true,
+      recursive: true,
+      maxChildDepth: 0,
+      continueOnError: true,
+      version: 0,
+      loadChildren: true,
+      id: null,
     }
   ) {
     this.token = await this.avatar.callLogin();
@@ -47,44 +47,46 @@ class Data {
         .catch(function (error) {
           return { error: true, data: error };
         });
-    }
-    const config = {
-      method: "post",
-      url: `https://api.oasisplatform.world/api/data/load-holon`,
-      headers: {
-        Authorization: `Bearer ${this.token.jwtToken}`,
-      },
-      data: JSON.stringify(data),
-    };
+    } else if (typeof data === "object") {
+      const config = {
+        method: "post",
+        url: `https://api.oasisplatform.world/api/data/load-holon`,
+        headers: {
+          Authorization: `Bearer ${this.token.jwtToken}`,
+        },
+        data: JSON.stringify(data),
+      };
 
-    return axios(config)
-      .then(function (response) {
-        if (response.data.isError) return { error: true, data: response.data };
-        else return { error: false, data: response.data };
-      })
-      .catch(function (error) {
-        return { error: true, data: error };
-      });
+      return axios(config)
+        .then(function (response) {
+          if (response.data.isError)
+            return { error: true, data: response.data };
+          else return { error: false, data: response.data };
+        })
+        .catch(function (error) {
+          return { error: true, data: error };
+        });
+    }
   }
 
   async loadAllHolons(
     data = {
-      HolonType: "Moon",
-      LoadChildren: true,
-      Recursive: true,
-      MaxChildDepth: 0,
-      ContinueOnError: false,
-      Version: 0,
-      ProviderType: "HoloOASIS",
-      SetGlobally: false,
-      ShowDetailedSettings: true,
-      AutoFailOverEnabled: "true",
-      AutoReplicationEnabled: "true",
-      AutoLoadBalanceEnabled: "true",
-      AutoFailOverProviders: "MongoDBOASIS, HoloOASOS",
-      AutoReplicationProviders: "MongoDBOASIS, HoloOASOS",
-      AutoLoadBalanceProviders: "MongoDBOASIS, HoloOASOS",
-      WaitForAutoReplicationResult: false,
+      providerType: "Default",
+      setGlobally: true,
+      autoFailOverMode: "DEFAULT",
+      autoReplicationMode: "DEFAULT",
+      autoLoadBalanceMode: "DEFAULT",
+      autoFailOverProviders: "DEFAULT",
+      autoReplicationProviders: "DEFAULT",
+      autoLoadBalanceProviders: "DEFAULT",
+      waitForAutoReplicationResult: false,
+      showDetailedSettings: true,
+      recursive: true,
+      maxChildDepth: 0,
+      continueOnError: true,
+      version: 0,
+      loadChildren: true,
+      id: null,
     }
   ) {
     this.token = await this.avatar.callLogin();
@@ -111,9 +113,10 @@ class Data {
         });
     }
 
+    else if (typeof data === "object"){
     const config = {
       method: "post",
-      url: `https://api.oasisplatform.world/api/data/load-all-holons/${holonType}`,
+      url: `https://api.oasisplatform.world/api/data/load-all-holons`,
       headers: {
         Authorization: `Bearer ${this.token.jwtToken}`,
       },
@@ -128,27 +131,27 @@ class Data {
       .catch(function (error) {
         return { error: true, data: error };
       });
+    }
   }
 
-  async loadHolonParent(
+  async loadHolonsForParent(
     data = {
-      Id: "89a935f4-d0ad-44af-b954-558041a76c18",
-      HolonType: "Moon",
-      LoadChildren: true,
-      Recursive: true,
-      MaxChildDepth: 0,
-      ContinueOnError: false,
-      Version: 0,
-      ProviderType: "HoloOASIS",
-      SetGlobally: false,
-      ShowDetailedSettings: true,
-      AutoFailOverEnabled: "true",
-      AutoReplicationEnabled: "true",
-      AutoLoadBalanceEnabled: "true",
-      AutoFailOverProviders: "MongoDBOASIS, HoloOASOS",
-      AutoReplicationProviders: "MongoDBOASIS, HoloOASOS",
-      AutoLoadBalanceProviders: "MongoDBOASIS, HoloOASOS",
-      WaitForAutoReplicationResult: false,
+      providerType: "Default",
+      setGlobally: true,
+      autoFailOverMode: "DEFAULT",
+      autoReplicationMode: "DEFAULT",
+      autoLoadBalanceMode: "DEFAULT",
+      autoFailOverProviders: "DEFAULT",
+      autoReplicationProviders: "DEFAULT",
+      autoLoadBalanceProviders: "DEFAULT",
+      waitForAutoReplicationResult: false,
+      showDetailedSettings: true,
+      recursive: true,
+      maxChildDepth: 0,
+      continueOnError: true,
+      version: 0,
+      loadChildren: true,
+      id: null,
     }
   ) {
     this.token = await this.avatar.callLogin();
@@ -157,7 +160,44 @@ class Data {
     if (typeof data === "string") {
       const config = {
         method: "get",
-        url: `https://api.oasisplatform.world/api/data/load-holons-for-parent/${data}/all`,
+        url: `https://api.oasisplatform.world/api/data/load-holons-for-parent/${data}`,
+        headers: {
+          Authorization: `Bearer ${this.token.jwtToken}`,
+        },
+      };
+
+      return axios(config)
+        .then(function (response) {
+          if (response.data.isError)
+            return { error: true, data: response.data };
+          else return { error: false, data: response.data };
+        })
+        .catch(function (error) {
+          return { error: true, data: error };
+        });
+    } else if (typeof data === "object") {
+      const config = {
+        method: "post",
+        url: `https://api.oasisplatform.world/api/data/load-holons-for-parent`,
+        headers: {
+          Authorization: `Bearer ${this.token.jwtToken}`,
+        },
+        data: JSON.stringify(data),
+      };
+
+      return axios(config)
+        .then(function (response) {
+          if (response.data.isError)
+            return { error: true, data: response.data };
+          else return { error: false, data: response.data };
+        })
+        .catch(function (error) {
+          return { error: true, data: error };
+        });
+    } else if (data === undefined) {
+      const config = {
+        method: "get",
+        url: `https://api.oasisplatform.world/api/data/load-holons-for-parent/all`,
         headers: {
           Authorization: `Bearer ${this.token.jwtToken}`,
         },
@@ -173,23 +213,6 @@ class Data {
           return { error: true, data: error };
         });
     }
-    const config = {
-      method: "post",
-      url: `https://api.oasisplatform.world/api/data/load-holons-for-parent`,
-      headers: {
-        Authorization: `Bearer ${this.token.jwtToken}`,
-      },
-      data: JSON.stringify(data),
-    };
-
-    return axios(config)
-      .then(function (response) {
-        if (response.data.isError) return { error: true, data: response.data };
-        else return { error: false, data: response.data };
-      })
-      .catch(function (error) {
-        return { error: true, data: error };
-      });
   }
 
   async saveHolon(data) {
